bcda66274654e34a3e6782d1cd4d1de75eb88aa2
destiny <desto4q@gmail.com>
2026-02-14 20:32:24 +0100

    order status

--- app/api/pocketbase.ts
+++ app/api/pocketbase.ts
@@ -1,3 +1,10 @@
-import PocketBase from "pocketbase";
+// import PocketBase from "pocketbase";
 
-export const pb = new PocketBase(import.meta.env.VITE_PB_URL);
+// export const pb = new PocketBase(import.meta.env.VITE_PB_URL);
+
+import { PocketBaseTS } from "pocketbase-ts";
+import type { TypedPocketBase } from "pocketbase-types";
+
+export const pb = new PocketBaseTS(
+  "http://127.0.0.1:8090",
+) as Partial<TypedPocketBase>;
